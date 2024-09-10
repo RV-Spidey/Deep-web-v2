b4b6b32bcdc853c7367a9951a52d585652f59d8e
RV-Spidey <killerkingmega10@gmail.com>
2024-09-11 00:41:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+const toggleButton = document.getElementById('toggle-btn')
+const sidebar = document.getElementById('sidebar')
+
+function toggleSidebar(){
+  sidebar.classList.toggle('close')
+  toggleButton.classList.toggle('rotate')
+
+  closeAllSubMenus()
+}
+
+function toggleSubMenu(button){
+
+  if(!button.nextElementSibling.classList.contains('show')){
+    closeAllSubMenus()
+  }
+
+  button.nextElementSibling.classList.toggle('show')
+  button.classList.toggle('rotate')
+
+  if(sidebar.classList.contains('close')){
+    sidebar.classList.toggle('close')
+    toggleButton.classList.toggle('rotate')
+  }
+}
+function showVideo(videoId) {
+  // Hide all videos
+  const videos = document.querySelectorAll('.video');
+  videos.forEach(video => video.style.display = 'none');
+
+  // Show the selected video
+  const videoToShow = document.getElementById(videoId);
+  if (videoToShow) {
+    videoToShow.style.display = 'block';
+  }
+}
+
+function closeAllSubMenus(){
+  Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
+    ul.classList.remove('show')
+    ul.previousElementSibling.classList.remove('rotate')
+  })
+}
